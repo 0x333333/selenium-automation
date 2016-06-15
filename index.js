df851441b4bd15d16c7859986216649d99a1c0bc
Zhipeng Jiang <zhipj@microsoft.com>
2016-06-15 18:20:37 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnG')).click();
